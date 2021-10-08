2bcf64bd589afe761f755dd26405af8db393942c
Said Ali <ali.said@esprit.tn>
2021-10-08 20:53:46 +0200

    first commit

---
+++ src/main/java/tn/esprit/spring/repository/DepartementRepository.java
@@ -0,0 +1,9 @@
+package tn.esprit.spring.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import tn.esprit.spring.entities.Departement;
+
+public interface DepartementRepository extends CrudRepository<Departement, Integer>{
+
+}
