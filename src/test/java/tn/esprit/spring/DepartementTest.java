1a81ba3be024eb398e4ef7e2d614e44042a52f6e
siwar20 <siwar.berzouga@esprit.tn>
2021-10-26 10:50:53 +0100

    add modification

--- src/test/java/tn/esprit/spring/DepartementTest.java
+++ src/test/java/tn/esprit/spring/DepartementTest.java
@@ -2,13 +2,15 @@ package tn.esprit.spring;
 
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
+
 import org.apache.logging.log4j.LogManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.repository.DepartementRepository;
 import tn.esprit.spring.services.DepartementServiceImpl;
@@ -18,10 +20,10 @@ import tn.esprit.spring.services.EmployeServiceImpl;
 @SpringBootTest
 public class DepartementTest {
 	private static final org.apache.logging.log4j.Logger l= LogManager.getLogger(DepartementServiceImpl.class);
-@Autowired
-DepartementServiceImpl departementServiceImpl;
-@Autowired
-DepartementRepository departementRepository;
+	@Autowired
+	DepartementServiceImpl departementServiceImpl;
+	@Autowired
+	DepartementRepository departementRepository;
 
 @Test
 public void contextLoads(){
@@ -48,6 +50,22 @@ public void testCreateDepartement(){
 	}
 }
 
+	@Test
+	public void testAddDepartement() throws ParseException {
+		Departement d = new Departement("DEP B");
+		Departement savedDepartement = departementRepository.save(d);
+		assertNotNull(savedDepartement.getId());
+		departementRepository.delete(savedDepartement);
+	}
 
+	@Test
+	public void testretrieveAllDepartement(){
+		departementServiceImpl.retrieveAllDepartement();
+	}
+	
+	
+	
+	
+	
 
 }
